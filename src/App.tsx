82a0bca71cd25f79e4544783a4429d7b7e667b03
Oskrvalencia <ovalencia08@misena.edu.co>
2023-07-03 20:24:22 -0500

    fix: my first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./../src/assets/styles/App.css";
+import Notfound404 from "./components/404";
+
+function App() {
+  return (
+    <>
+      <Notfound404 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/404.tsx
@@ -0,0 +1,32 @@
+import './../assets/styles/components.css'
+
+const Notfound404 = () => {
+  return (
+    <div className="notfound">
+      <div className="row">
+        <h4 className="title">404 NOT FOUND</h4>
+      </div>
+      <div className="row">
+        <img
+          src="src/assets/images/Scarecrow.png"
+          alt=""
+          className="col-md-6 col-sm-12"
+        />
+        <div className="col-md-6 col-sm-12">
+          <h1>I have bad news for you</h1>
+          <p className='col-7'>
+            The page you are looking for might be removed or is remporarily
+            unavailable
+          </p>
+          <button>BACK TO HOME PAGE</button>
+        </div>
+      </div>
+
+      <footer>created by <strong>ovalencia</strong> - devChallenges.io</footer>
+    </div>
+  );
+};
+
+Notfound404.propTypes = {};
+
+export default Notfound404;
